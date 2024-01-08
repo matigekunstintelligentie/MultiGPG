bc5472ee2fee49624fb866a9c02955ca7151f8f0
Matigekunstintelligentie <Matigekunstintelligentie@gmail.com>
2024-01-08 16:44:29 +0100

    Adding kleader clustering and elitist archive

---
+++ src/elitist_archive.hpp
@@ -0,0 +1,100 @@
+#include "globals.hpp"
+#include "individual.hpp"
+
+using namespace std;
+
+struct ElitistArchive{
+    vector<Individual*> SO_archive;
+    vector<Individual*> MO_archive;
+
+    bool dominates(Individual * ind1, Individual * ind2){
+        bool strictly_better_somewhere = false;
+        for(int i = 0; i<2; i++){
+            if(ind1->fitness[i] < ind2->fitness[i]){
+                strictly_better_somewhere = true;
+            }
+            else if(ind1->fitness[i] > ind2->fitness[i]){
+                return false;
+            }
+        }
+        return strictly_better_somewhere;
+    }
+
+    bool diversityAdded(Individual* individual, vector<Node*> & trees, int idx){
+        Vec diff = individual->get_output(g::fit_func->X_train, trees) - MO_archive[idx]->get_output(g::fit_func->X_train, trees);
+        if(diff.mean()==0){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+
+    void initSOArchive(vector<Individual*> population, vector<Node*> & trees){
+        for(Individual *ind: population){
+            updateSOArchive(ind, trees);
+        }
+    }
+
+    void initMOArchive(vector<Individual*> population, vector<Node*> & trees){
+        for(Individual *ind: population){
+            updateSOArchive(ind, trees);
+        }
+    }
+
+    void updateSOArchive(Individual * individual, vector<Node*> & trees){
+        for(int i=0; i<2; i++){
+            if(SO_archive[i] == nullptr){
+                Individual *new_individual = individual->clone();
+                SO_archive[i] = new_individual;
+            }
+            else if(SO_archive[i]->fitness[i] > individual->fitness[i]){
+                SO_archive[i]->clear();
+                SO_archive[i] = nullptr;
+                Individual *new_individual = individual->clone();
+                SO_archive[i] = new_individual;
+            }
+        }
+    }
+
+    void updateMOArchive(Individual * individual, vector<Node*> & trees){
+        bool solution_is_dominated = false;
+        bool diversity_added = false;
+        bool identical_objectives_already_exist;
+
+        for(int i = 0; i<MO_archive.size(); i++){
+            solution_is_dominated = dominates(MO_archive[i], individual);
+            if(solution_is_dominated){
+                break;
+            }
+
+            identical_objectives_already_exist = true;
+            for(int j=0; j<2; j++){
+                if(individual->fitness[j] != MO_archive[i]->fitness[j]){
+                    identical_objectives_already_exist = false;
+                    break;
+                }
+            }
+            if(identical_objectives_already_exist){
+                if (diversityAdded(individual, trees, i)) {
+                    diversity_added = true;
+                    MO_archive[i]->clear();
+                    MO_archive[i] = nullptr;
+                }
+                break;
+            }
+
+            if(dominates(individual, MO_archive[i])){
+                MO_archive[i]->clear();
+                MO_archive[i] = nullptr;
+            }
+        }
+        MO_archive.erase(std::remove_if(MO_archive.begin(), MO_archive.end(), [](Individual *ind){return ind== nullptr;}), MO_archive.end());
+
+        if ((!solution_is_dominated && !identical_objectives_already_exist) || (diversity_added)) {
+            Individual *new_individual = individual->clone();
+            g::fit_func->get_fitness_MO(new_individual);
+            MO_archive.push_back(new_individual);
+        }
+    }
+};
--- src/evolution.hpp
+++ src/evolution.hpp
@@ -88,6 +88,219 @@ struct Evolution {
     ++gen_number;
   }
 
+  pair<pair<vector<vector<Node *>>, vector<vector<Node *>>>, vector<size_t>>
+  K_leader_means(vector<Individual *> &population) {
+      int k = 7;
+      int pop_size = population.size();
+
+      // keep track of possible leaders
+      vector<int> range(pop_size);
+      iota(range.begin(), range.end(), 0);
+      // all solutions are still possible as leaders
+      unordered_set<int> remaining_solutions(range.begin(), range.end());
+
+      // get objective value data and normalise
+      int nr_objs = 2;
+      Mat norm_data(nr_objs, pop_size);
+      for(int i=0; i<pop_size; i++){
+          for(int j=0; j<nr_objs; j++){
+              norm_data(j,i) = population[i]->fitness[j];
+          }
+      }
+
+      for(int i=0; i<nr_objs; i++){
+          float min;
+          bool min_initialised = false;
+          float max;
+          bool max_initialised = false;
+          // get min and max for objective
+          for(int x: remaining_solutions){
+              float value = norm_data(i, x);
+              if (!min_initialised || value < min) {
+                  min = value;
+                  min_initialised = true;
+              }
+              if (!min_initialised || value > max) {
+                  max = value;
+                  min_initialised = true;
+              }
+          }
+          //normalize each value in population for that objective
+          for (int j = 0; j < pop_size; j++) {
+              norm_data(i, j) = (norm_data(i, j) - min) / ((max - min)+0.0000000001);
+          }
+      }
+      // get first leaders based on smallest objective value
+      int initialised_k = 0;
+      vector<int> idx_leaders;
+      // take a random objective
+      int random_obj = round(randu()*(nr_objs-1));
+
+      int idx_min = *remaining_solutions.begin();
+      for(int x: remaining_solutions){
+          if(norm_data(random_obj, idx_min)>norm_data(random_obj,x)){
+              idx_min = x;
+          }
+      }
+      int first_leader = idx_min;
+      idx_leaders.push_back(first_leader);
+      remaining_solutions.erase(first_leader);
+      initialised_k++;
+
+      // find other leaders
+      // first get distance of all remaining possible leaders to first leaders
+      Vec dists = Zero(pop_size);
+      for(int x: remaining_solutions){
+          dists(x) = (norm_data.col(x) - norm_data.col(idx_leaders[0])).square().mean();
+      }
+
+      while(initialised_k<k && !remaining_solutions.empty()){
+          // select leader with longest distance to other leaders
+          int new_leader = dist.index_max();
+          idx_leaders.push_back(new_leader);
+          initialised_k++;
+          // minimum distance of new leader to other leaders now becomes 0 and the new leader is not a remaining possible leader
+          dists[new_leader] = 0;
+          remaining_solutions.erase(new_leader);
+          // update distances of possible new leaders: if the new solution is closer to the individual then the one currently closest we update the distance
+          for (size_t x: remaining_solutions) {
+              double minimum = min((norm_data.col(x) - norm_data.col(new_leader)).square().mean(), dists[x]);
+              dists[x] = minimum;
+          }
+      }
+
+      // intialize cluster tags for k-means
+      vector<int> clustertags_kmeans(pop_size);
+      // intialize bool for whether there was a change in cluster assignments
+      bool cluster_change = true;
+      // initialize matrix for centers of the clusters (start with location of the leaders)
+      Mat cluster_centers = Zeros(nr_objs, idx_leaders.size());
+      for(int i =0; i<idx_leaders.size(); i++){
+          cluster_centers.col(i) = norm_data.col(idx_leaders[i]);
+      }
+
+      // k-means
+      int iter = 0;
+      while(cluster_change && iter<50){
+          cluster_change = false;
+          Mat cluster_centers_temp = Zeros(nr_objs, idx_leaders.size());
+          vector<int> count = vector<int>(idx_leaders.size(), 0);
+
+          for(int i = 0; i<pop_size; i++){
+              bool lowest_dist_init = false;
+              float lowest_dist;
+              int index_min;
+              // check the center that is closest for this indivdual
+              for (size_t j = 0; j < idx_leaders.size(); j++) {
+                  if (!lowest_dist_init ||
+                      lowest_dist > (norm_data.col(i) - cluster_centers.col(j)).square().mean()) {
+                      lowest_dist = (norm_data.col(i) - cluster_centers.col(j)).square().mean();
+                      index_min = j;
+                      lowest_dist_init = true;
+                  }
+              }
+              // if it is different than before we update it
+              if (index_min != clustertags_kmeans[i]) {
+                  clustertags_kmeans[i] = index_min;
+                  cluster_change = true;
+              }
+              // cluster_centers_temp are updated as well as the number of individuals belonging to a center, for calculating the new clustercenters later on
+              cluster_centers_temp.col(clustertags_kmeans[i]) += norm_data.col(i);
+              counts[clustertags_kmeans[i]] += 1;
+          }
+          for(int nr_lead=0; nr_lead<idx_leaders.size(); nr_lead++){
+              // if the center doesn't have any individuals, reset to position leader
+              if (counts[nr_lead] == 0) {
+                  cluster_centers.col(nr_lead) = norm_data.col(idx_leaders[nr_lead]);
+                  clustertags_kmeans[idx_leaders[nr_lead]] = nr_lead;
+              }
+              // else finalize new cluster center by calculating sum values of all individuals divided by the number of individuals of that cluster
+              else{
+                  for (size_t obj = 0; obj < nr_objs; obj++) {
+                      cluster_centers(obj, nr_lead) = cluster_centers_temp(obj, nr_lead) / counts[nr_lead];
+                  }
+              }
+          }
+          iter++;
+      }
+
+      vector<vector<int>> clustertags_equal = vector<vector<int>>(pop_size,vector<int>());
+      // store the solutions of each cluster in a separate vector for FOS in clustered_population
+      vector<vector<Individual *>> clustered_population = vector<vector<Individual *>>(initalised_k,vector<Individual *>());
+      vector<vector<Individual *>> clustered_population_equal = vector<vector<Individual * >>(initalised_k,vector<Node *>());
+
+      // assign to each cluster the closests 2*pop_size/cluster solutions
+      for(int x=0; x<initialised_k; x++){
+          Vec distances = Vec(pop_size);
+          for(int i = 0; i<pop_size; i++){
+              distances[i] = (norm_data.col(i) - cluster_centers.col(x)).square().mean();
+          }
+          for(int times=0; times<((2*pop_size)/initialised_k); times++){
+              clustered_population_equal[x].push_back(population[distances.index_min()]);
+              clustertags_equal[distances.index_min()].push_back(x);
+              distances[distances.index_min()] = inf;
+          }
+      }
+
+      vector<int> clusternr = vector<int>(intialised_k, nr_objs + 1);
+
+      for(int i=0; i<nr_objs; i++){
+          clusternr[cluster_centers.row(i).index_min()] = i;
+      }
+
+      // if not jet assigned solution in equal size clustering, assign to closest. if multiple are assigned, assign to random of multiple center
+      for (size_t i = 0; i < pop_size; i++) {
+          
+      }
+
+  }
+
+  void gomea_MO_generation(int macro_generation){
+      vector<Individual*> offspring_population;
+
+      int nr_objectives = 2;
+      int NIS = 1 + log10(g::pop_size);
+
+      // Make clusters
+      pair<pair<vector<vector<Individual*>>, vector<vector<Individual*>>>, vector<int>> output = K_leader_means(population);
+      vector<vector<Individual *>> clustered_population = output.first.first;
+      vector<vector<Individual *>> clustered_population_equal = output.first.second;
+      vector<int> clusternr = output.second;
+
+      std::vector<vector<vector<int>>> FOSs;
+      for(int i=0; i<clustered_population.size();i++){
+          FOSs.push_back(fb->build_linkage_tree(clustered_population_equal[i]);
+      }
+
+      vector<pair<int, int>> idx;
+      for(int i=0;i<clustered_population.size();i++){
+          for(int j=0;j<clustered_population.size();j++){
+              idx.emplace_back(i, j);
+          }
+      }
+
+      for(int x=0; x<idx.size(); x++){
+          int &i = idx[x].first;
+          int &j = idx[x].second;
+
+          Individual *offspring;
+          if(clustered_population.size()>1){
+              offspring = GOMMO();
+          }
+          else{
+              offspring = GOMMO();
+          }
+          offspring_population.push_back(offspring);
+      }
+
+      assert(offspring_population.size()==population.size());
+
+      for(int i=0; i<population.size(); i++){
+          population[i]->clear();
+      }
+
+      population = offspring_population;
+  }
 
 
   void run() {
